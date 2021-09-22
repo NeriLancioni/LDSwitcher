13b62e1ff56042b2b177bda1fa052b9efabc6107
Neri Lancioni <62160943+NeriLancioni@users.noreply.github.com>
2021-09-22 19:29:17 -0300

    Minor changes
    -A single instance of this script can run at the same time to avoid bugs.
    -"setlocal enabledelayedexpansion" no longer needed.
    -"!var!" replaced with "%var%".

--- LDSwitcher.bat
+++ LDSwitcher.bat
@@ -1,12 +1,12 @@
 @echo off
-setlocal enabledelayedexpansion
 
 REM If script is being run from vbs script, go to background task
 if "%1"=="task" goto :SWITCHER
+title LDSwitcher
 
 REM Detect language
 call :findLang es-
-if "!errorlevel!"=="0" (
+if "%errorlevel%"=="0" (
     call :setLangEs
     goto :langSelected
 )
@@ -16,31 +16,42 @@ call :setLangEn
 :langSelected
 
 call :clearDualEcho
-echo !strInit!
+echo %strInit%
 
 REM Check Windows version
 wmic os get Caption /value | find "Windows 10" >nul 2>&1
-if !errorlevel! neq 0 (
+if %errorlevel% neq 0 (
     call :clearDualEcho
-    echo !strNotTen1!
-    echo !strNotTen2!
+    echo %strNotTen1%
+    echo %strNotTen2%
     echo.
-	echo !strExit!
+	echo %strExit%
 	pause > nul
 	exit /b 1
 )
 
 REM Check for admin privileges
 mkdir %windir%\checkYourPrivileges >nul 2>&1
-if "!errorlevel!"=="0" (
+if "%errorlevel%"=="0" (
 	rmdir /s /q %windir%\checkYourPrivileges
     call :clearDualEcho
-    echo !strUAC1!
-	echo !strUAC2!
+    echo %strUAC1%
+	echo %strUAC2%
 	echo.
-	echo !strExit!
+	echo %strExit%
 	pause > nul
 	exit /b 2
+)
+
+for /f "delims=" %%i in ('tasklist /fi "WINDOWTITLE eq LDSwitcher" /fo csv /nh ^| find /c /i "cmd.exe"') do (
+    if "%%i" neq "1" (
+        call :clearDualEcho
+        echo %strMultiInstances%
+        echo.
+        echo %strExit%
+        pause > nul
+        exit /b 3
+    )
 )
 
 call :cmdColor
@@ -50,28 +61,28 @@ if not exist "%localappdata%\LDSwitcher\" goto :install
 
 REM If installed, ask if user wants to modify, uninstall or cancel
 call :clearDualEcho
-echo !strAlreadyInstalled!
-choice /c !strWhatToDoOptions! /n /m !strWhatToDo!
-if "!errorlevel!"=="1" goto :install
-if "!errorlevel!"=="2" goto :uninstall
-if "!errorlevel!"=="3" exit /b
+echo %strAlreadyInstalled%
+choice /c %strWhatToDoOptions% /n /m %strWhatToDo%
+if "%errorlevel%"=="1" goto :install
+if "%errorlevel%"=="2" goto :uninstall
+if "%errorlevel%"=="3" exit /b
 exit /b
 
 :install
 REM Define light mode start time (24hs format)
-call :timeInput !strLightStart!
-set lightStart=!timeAcu!
+call :timeInput %strLightStart%
+set lightStart=%timeAcu%
 
 REM Define dark mode start time (24hs format)
-call :timeInput !strDarkStart!
-set darkStart=!timeAcu!
+call :timeInput %strDarkStart%
+set darkStart=%timeAcu%
 
 REM Check if light mode starts before dark mode
-set /a lightStartMin=!lightStart:~0,2!*60+!lightStart:~3,2!
-set /a darkStartMin=!darkStart:~0,2!*60+!darkStart:~3,2!
-if !darkStartMin! leq !lightStartMin! (
+set /a lightStartMin=%lightStart:~0,2%*60+%lightStart:~3,2%
+set /a darkStartMin=%darkStart:~0,2%*60+%darkStart:~3,2%
+if %darkStartMin% leq %lightStartMin% (
     call :clearDualEcho
-    echo !strLDWrongOrder!
+    echo %strLDWrongOrder%
     echo.
     pause
     goto :install
@@ -79,51 +90,53 @@ if !darkStartMin! leq !lightStartMin! (
 
 REM Define task bar behaviour
 call :clearDualEcho
-echo !strTaskBarModeMsg!
-echo !strTaskBarOptLight!
-echo !strTaskBarOptDark!
-echo !strTaskBarOptByTime!
+echo %strTaskBarModeMsg%
+echo %strTaskBarOptLight%
+echo %strTaskBarOptDark%
+echo %strTaskBarOptByTime%
 echo.
-choice /c !strTaskBarOptions! /n
-set /a barMode=!errorlevel!-1
+choice /c %strTaskBarOptions% /n
+set /a barMode=%errorlevel%-1
 
 
 call :clearDualEcho
-echo !strInstalling!
+echo %strInstalling%
 
-REM Delete LDSwitcher folder for 6 seconds to stop previous instances
-rmdir /s /q "%localappdata%\LDSwitcher\" >nul 2>&1
-timeout /t 6 /nobreak > nul
+REM Stop previous instance
+taskkill /fi "WINDOWTITLE eq LDSwitcher Background Process" /f >nul 2>&1
 
 REM Copy required files to separate folder
 mkdir "%localappdata%\LDSwitcher" >nul 2>&1
 copy /y %0 "%localappdata%\LDSwitcher\LDSwitcher.bat" >nul 2>&1
 
-REM Create startup script
-echo Set WshShell = WScript.CreateObject("WScript.Shell") > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\LDSwitcher.vbs"
-echo WshShell.Run """%localappdata%\LDSwitcher\LDSwitcher.bat"" task", 0, False >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\LDSwitcher.vbs"
+REM Create silent start script
+echo Set WshShell = WScript.CreateObject("WScript.Shell") > "%localappdata%\LDSwitcher\LDSwitcher.vbs"
+echo WshShell.Run """%localappdata%\LDSwitcher\LDSwitcher.bat"" task", 0, False >> "%localappdata%\LDSwitcher\LDSwitcher.vbs"
 
-REM Create config.txt
-echo lightTime=!lightStartMin! > "%localappdata%\LDSwitcher\config.txt"
-echo darkTime=!darkStartMin! >> "%localappdata%\LDSwitcher\config.txt"
-echo taskBarMode=!barMode! >> "%localappdata%\LDSwitcher\config.txt"
+REM Create startup registry
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v LDSwitcher /t REG_SZ /d "\"%localappdata%\LDSwitcher\LDSwitcher.vbs\"" /f >nul 2>&1
+
+REM Save settings to registry
+reg add "HKCU\SOFTWARE\NeriLancioni\LDSwitcher" /v lightTime /t REG_SZ /d %lightStartMin% /f >nul 2>&1
+reg add "HKCU\SOFTWARE\NeriLancioni\LDSwitcher" /v darkTime /t REG_SZ /d %darkStartMin% /f >nul 2>&1
+reg add "HKCU\SOFTWARE\NeriLancioni\LDSwitcher" /v taskBarMode /t REG_SZ /d %barMode% /f >nul 2>&1
 
 REM Run theme changer from startup script
-"%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\LDSwitcher.vbs" >nul 2>&1
+"%localappdata%\LDSwitcher\LDSwitcher.vbs" >nul 2>&1
 
 REM Wait 5 seconds for background process to change current windows theme
-timeout /t 5 /nobreak > nul
+choice /t 5 /c ab /d a > nul
 call :cmdColor
 
 REM Success message and exit
 call :clearDualEcho
-echo !strInstallSuccess!
+echo %strInstallSuccess%
 echo.
-echo !strLightStartAt! !lightStart!
-echo !strDarkStartAt! !darkStart!
-if !barMode!==0 ( echo !strBarIsDark! )
-if !barMode!==1 ( echo !strBarIsLight! )
-if !barMode!==2 ( echo !strBarIsAuto! )
+echo %strLightStartAt% %lightStart%
+echo %strDarkStartAt% %darkStart%
+if %barMode%==0 ( echo %strBarIsDark% )
+if %barMode%==1 ( echo %strBarIsLight% )
+if %barMode%==2 ( echo %strBarIsAuto% )
 echo.
 echo.
 pause
@@ -132,12 +145,10 @@ exit /b
 
 
 :SWITCHER
-REM Check configuration file existence
-if not exist "%localappdata%\LDSwitcher\config.txt" exit /b 2
-REM Load parameters from config file
-for /f "tokens=1,2 delims==" %%a in ('more "%localappdata%\LDSwitcher\config.txt"') do (
-    set /a %%a=%%b
-)
+title LDSwitcher Background Process
+
+REM Load parameters from registry
+for /f "skip=2 tokens=1,3 delims= " %%a in ('reg query HKCU\SOFTWARE\NeriLancioni\LDSwitcher') do set /a %%a=%%b
 
 REM Check if needed variables are defined
 if not defined lightTime exit /b 3
@@ -151,21 +162,21 @@ if not defined taskBarMode exit /b 5
     REM Set light mode if current time is between light and dark mode start times
     REM If not, set dark mode
     set mode=0
-    if !now! geq !lightTime! (
-        if !now! leq !darkTime! (
+    if %now% geq %lightTime% (
+        if %now% leq %darkTime% (
             set mode=1
         )
     )
 
     rem Change system colors
-    call :setTheme AppsUseLightTheme !mode!
+    if %taskBarMode%==2 (
+        call :setTheme SystemUsesLightTheme %mode%
+    ) else (
+        call :setTheme SystemUsesLightTheme %taskBarMode%
+    )
 
     rem Change apps colors
-    if !taskBarMode!==2 (
-        call :setTheme SystemUsesLightTheme !mode!
-    ) else (
-        call :setTheme SystemUsesLightTheme !taskBarMode!
-    )
+    call :setTheme AppsUseLightTheme %mode%
 
     choice /t 5 /c ab /d a > nul
 goto :loop
@@ -182,21 +193,26 @@ exit /b
 
 :uninstall
 call :clearDualEcho
-choice /c !strYesNo! /n /m !strUninstallQuestion!
-if "!errorlevel!" neq "1" (
+choice /c %strYesNo% /n /m %strUninstallQuestion%
+if "%errorlevel%" neq "1" (
     call :clearDualEcho
-    echo !strUninstallCancel!
+    echo %strUninstallCancel%
     echo.
     pause
     exit /b
 )
 call :clearDualEcho
-echo !strUninstalling!
-del /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\LDSwitcher.vbs" >nul 2>&1
+echo %strUninstalling%
+taskkill /fi "WINDOWTITLE eq LDSwitcher Background Process" /f >nul 2>&1
+timeout /t 1 /nobreak > nul
 rmdir /s /q "%localappdata%\LDSwitcher\" >nul 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v LDSwitcher /f >nul 2>&1
+reg delete "HKCU\SOFTWARE\NeriLancioni\LDSwitcher" /f >nul 2>&1
+reg query "HKCU\SOFTWARE\NeriLancioni" | find "\" >nul 2>&1
+if "%errorlevel%" neq 0 reg delete "HKCU\SOFTWARE\NeriLancioni" /f >nul 2>&1
 
 call :clearDualEcho
-echo !strUninstallSuccess!
+echo %strUninstallSuccess%
 echo.
 pause
 exit /b
@@ -204,22 +220,22 @@ exit /b
 :timeInput
 set timeAcu=
 call :timeInput2 012 %1
-if "!timeAcu!"=="2" (
+if "%timeAcu%"=="2" (
     call :timeInput2 0123 %1
 ) else (
     call :timeInput2 0123456789 %1
 )
-set timeAcu=!timeAcu!:
+set timeAcu=%timeAcu%:
 call :timeInput2 012345 %1
 call :timeInput2 0123456789 %1
 exit /b
 :timeInput2
 set message=%2
 call :clearDualEcho
-echo !message:~1,-1!!timeAcu!
+echo %message:~1,-1%%timeAcu%
 choice /c %1 /n
-set /a indexOffset=!errorlevel!-1
-set timeAcu=!timeAcu!!indexOffset!
+set /a indexOffset=%errorlevel%-1
+set timeAcu=%timeAcu%%indexOffset%
 set indexOffset=
 exit /b
 
@@ -239,8 +255,8 @@ if "%1"=="" exit /b 1
 if not defined currentLang (
     for /f delims^=^"^ tokens^=2 %%i in ('wmic os get MUILanguages /value ^| find "MUILanguages"') do set currentLang=%%i
 )
-echo !currentLang! | find /i "%1" > nul
-exit /b !errorlevel!
+echo %currentLang% | find /i "%1" > nul
+exit /b %errorlevel%
 
 :setLangEs
 set strInit=Inicializando . . .
@@ -272,6 +288,7 @@ set strTaskBarOptions=OCH
 set strBarIsDark=La barra de tareas sera siempre oscura
 set strBarIsLight=La barra de tareas sera siempre clara
 set strBarIsAuto=La barra de tareas cambiara de color segun el horario
+set strMultiInstances=No puede haber mas de una instancia de LDSwitcher simultaneamente.
 exit /b
 
 :setLangEn
@@ -304,4 +321,5 @@ set strTaskBarOptions=DLT
 set strBarIsDark=Task bar will always be dark
 set strBarIsLight=Task bar will always be light
 set strBarIsAuto=Task bar will change depending on time
+set strMultiInstances=There cannot be more than one LDSwitcher instance simultaneously.
 exit /b
